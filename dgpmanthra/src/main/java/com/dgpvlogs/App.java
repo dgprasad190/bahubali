68eeb3d7df1c240f661a6083af107503c3a056d5
dgprasad <gourip092@gmail.com>
2023-12-03 13:16:57 +0530

    gyugugg

---
+++ dgpmanthra/src/main/java/com/dgpvlogs/App.java
@@ -0,0 +1,13 @@
+package com.dgpvlogs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
